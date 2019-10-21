26f41fbaff17017ee5baababdd55bb2d05bd24eb
vinicius molina <vinicius_molina99@hotmail.com>
2019-10-21 00:29:18 -0300

    fix postgresql

--- default.nix
+++ default.nix
@@ -153,11 +153,12 @@
 
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_10;
+    package = pkgs.postgresql_11;
     enableTCPIP = false;
-    authentication = pkgs.lib.mkOverride 10 ''
+    authentication = pkgs.lib.mkOverride 11 ''
       local all all trust
-      #host all all ::1/128 trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres' CREATEDB;
